6e08c75c4c922ff621bbf7aa72670fa76ae77e57
Mike Fiedler <miketheman@gmail.com>
2014-12-04 09:20:58 -0500

    require app name variable to set as hostname

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -3,7 +3,14 @@
 if [[ $DATADOG_API_KEY ]]; then
   sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
 else
-  echo "You must set DATADOG_API_KEY environment variable to run DogStatsD process"
+  echo "DATADOG_API_KEY environment variable not set. Run: heroku config:add DATADOG_API_KEY=<your API key>"
+  exit 1
+fi
+
+if [[ $HEROKU_APP_NAME ]]; then
+  sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+else
+  echo "HEROKU_APP_NAME environment variable not set. Run: heroku apps:info|grep ===|cut -d' ' -f2"
   exit 1
 fi
 
