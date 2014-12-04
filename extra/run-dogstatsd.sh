57112db59a875665d19151af1dbe9d2156b3d80e
Mike Fiedler <miketheman@gmail.com>
2014-12-04 08:29:02 -0500

    Initial commit

---
+++ extra/run-dogstatsd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $API_KEY ]]; then
+  sed -i -e "s/^.*api_key:.*$/api_key: ${API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+else
+  echo "You must set API_KEY environment variable to run DogStatsD process"
+  exit 1
+fi
+
+exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py
