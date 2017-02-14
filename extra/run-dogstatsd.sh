597b3c09fde5113c123fd90b6e63d6008681b0b0
Anton Ilin <anton@ilin.dn.ua>
2017-02-14 14:36:05 +0200

    Disable DataDog agent in the proper way

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ $DISABLE_DATADOG_AGENT ]]; then
-  echo "DISABLE_DATADOG_AGENT environment variable is set, not starting the agent."
-  exit 0
-fi
-
 if [[ $DATADOG_API_KEY ]]; then
   sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
 else
@@ -24,10 +19,14 @@ if [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
 fi
 
 (
-  # Unset other PYTHONPATH/PYTHONHOME variables before we start
-  unset PYTHONHOME PYTHONPATH
-  # Load our library path first when starting up
-  export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
-  mkdir -p /tmp/logs/datadog
-  exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
+  if [[ $DISABLE_DATADOG_AGENT ]]; then
+    echo "DISABLE_DATADOG_AGENT environment variable is set, not starting the agent."
+  else
+    # Unset other PYTHONPATH/PYTHONHOME variables before we start
+    unset PYTHONHOME PYTHONPATH
+    # Load our library path first when starting up
+    export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
+    mkdir -p /tmp/logs/datadog
+    exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
+  fi
 )
