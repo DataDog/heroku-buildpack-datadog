8d295acef189d468a1fa0a7e5a31f630ae1dcd11
Jason Yee <jason.yee@datadoghq.com>
2017-03-16 17:58:48 -0700

    Add DD_ var name support, add APM support

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-if [[ $DATADOG_API_KEY ]]; then
-  sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+# Prefered Datadog env var name
+if [[ $DD_API_KEY ]]; then
+  sed -i -e "s/^[# ]*api_key:.*$/api_key: ${DD_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+# Legacy env var support
+elif [[ $DATADOG_API_KEY ]]; then
+  sed -i -e "s/^[# ]*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
 else
-  echo "DATADOG_API_KEY environment variable not set. Run: heroku config:add DATADOG_API_KEY=<your API key>"
+  echo "DD_API_KEY environment variable not set. Run: heroku config:add DD_API_KEY=<your API key>"
   exit 1
 fi
 
-if [[ $HEROKU_APP_NAME ]]; then
-  sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+# Prefered Datadog env var name
+if [[ $DD_HOSTNAME ]]; then
+  sed -i -e "s/^[# ]*hostname:.*$/hostname: ${DD_HOSTNAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+# Legacy env var support
+elif [[ $HEROKU_APP_NAME ]]; then
+  sed -i -e "s/^[# ]*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
 else
-  echo "HEROKU_APP_NAME environment variable not set. Run: heroku apps:info|grep ===|cut -d' ' -f2"
+  echo "DD_HOSTNAME environment variable not set. Run: heroku config:set DD_HOSTNAME=$(heroku apps:info|grep ===|cut -d' ' -f2)"
   exit 1
 fi
 
-if [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
-  sed -i -e "s/^.*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+
+if [[ $DD_APM_ENABLED == "true" ]]; then
+  sed -i -e "s/^[# ]*apm_enabled:.*$/apm_enabled: true/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  echo "apm_enabled: true" >> /app/.apt/opt/datadog-agent/agent/datadog.conf
+fi
+
+# Prefered Datadog env var name
+if [[ $DD_HISTOGRAM_PERCENTILES ]]; then
+  sed -i -e "s/^[# ]*histogram_percentiles:.*$/histogram_percentiles: ${DD_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+# Legacy env var support
+elif [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
+  sed -i -e "s/^[# ]*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
 fi
 
 (
@@ -28,5 +46,16 @@ fi
     export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
     mkdir -p /tmp/logs/datadog
     exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
+  fi
+)
+
+(
+  if [[ $DISABLE_DATADOG_AGENT ]]; then
+    echo "DISABLE_DATADOG_AGENT environment variable is set, not starting the tracing agent."
+  else
+    # Enable the trace agent
+    if [[ $DD_APM_ENABLED == "true" ]]; then
+      exec /app/.apt/opt/datadog-agent/bin/trace-agent -ddconfig /app/.apt/opt/datadog-agent/agent/datadog.conf -debug >> /tmp/logs/datadog/trace-agent.log 2>&1 &
+    fi
   fi
 )
