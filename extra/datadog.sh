38999333a62c3e79f4bbd9ff38ed7dd1dd75a79f
Jason Yee <jason.yee@datadoghq.com>
2018-10-23 12:25:59 -0400

    Bringing in line with shellcheck

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -20,11 +20,11 @@ export DD_LOG_FILE="$DD_LOG_DIR/datadog.log"
 DD_APM_LOG="$DD_LOG_DIR/datadog-apm.log"
 
 # Move Datadog config files into place
-cp $DATADOG_CONF.example $DATADOG_CONF
+cp "$DATADOG_CONF.example" "$DATADOG_CONF"
 
 # Update the Datadog conf yaml with the correct conf.d and checks.d
-sed -i -e"s|^.*confd_path:.*$|confd_path: $DD_CONF_DIR/conf.d|" $DATADOG_CONF
-sed -i -e"s|^.*additional_checksd:.*$|additional_checksd: $DD_DIR/checks.d|" $DATADOG_CONF
+sed -i -e"s|^.*confd_path:.*$|confd_path: $DD_CONF_DIR/conf.d|" "$DATADOG_CONF"
+sed -i -e"s|^.*additional_checksd:.*$|additional_checksd: $DD_DIR/checks.d|" "$DATADOG_CONF"
 
 # Include application's datadog configs
 APP_DATADOG_CONF_DIR="/app/datadog/conf.d"
@@ -34,7 +34,7 @@ for file in "$APP_DATADOG_CONF_DIR"/*.yaml; do
   filename=$(basename -- "$file")
   filename="${filename%.*}"
   mkdir -p "$DD_CONF_DIR/conf.d/${filename}.d"
-  cp $file "$DD_CONF_DIR/conf.d/${filename}.d/conf.yaml"
+  cp "$file" "$DD_CONF_DIR/conf.d/${filename}.d/conf.yaml"
 done
 
 # Add tags to the config file
@@ -48,21 +48,21 @@ fi
 
 # Convert comma delimited tags from env vars to yaml
 if [ -n "$DD_TAGS" ]; then
-  DD_TAGS=$(sed "s/,[ ]\?/\\\n  - /g" <<< $DD_TAGS)
+  DD_TAGS=$(sed "s/,[ ]\?/\\\n  - /g" <<< "$DD_TAGS")
   TAGS="$TAGS\n  - $DD_TAGS"
   # User set tags are now in YAML, clear the env var.
   export DD_TAGS=""
 fi
 
 # Inject tags after example tags.
-sed -i "s/^#   - role:database$/#   - role:database\n$TAGS/" $DATADOG_CONF
+sed -i "s/^#   - role:database$/#   - role:database\n$TAGS/" "$DATADOG_CONF"
 
 # Uncomment APM configs and add the log file location.
-sed -i -e"s|^# apm_config:$|apm_config:\n    log_file: $DD_APM_LOG|" $DATADOG_CONF
+sed -i -e"s|^# apm_config:$|apm_config:\n    log_file: $DD_APM_LOG|" "$DATADOG_CONF"
 
 # Uncomment the Process Agent configs and enable.
 if [ "$DD_PROCESS_AGENT" == "true" ]; then
-  sed -i -e"s|^# process_config:$|process_config:\n    enabled: $DD_PROCESS_AGENT|" $DATADOG_CONF
+  sed -i -e"s|^# process_config:$|process_config:\n    enabled: true|" "$DATADOG_CONF"
 fi
 
 # For a list of env vars to override datadog.yaml, see:
@@ -76,16 +76,17 @@ fi
 if [ -z "$DD_HOSTNAME" ]; then
   if [ "$DD_DYNO_HOST" == "true" ]; then
     # Set the hostname to dyno name and ensure rfc1123 compliance.
-    HAN=$( echo $HEROKU_APP_NAME | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
+    HAN=$( echo "$HEROKU_APP_NAME" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
     if [ "$HAN" != "$HEROKU_APP_NAME" ]; then
       echo "WARNING: The appname \"$HEROKU_APP_NAME\" contains invalid characters. Using \"$HAN\" instead."
     fi
 
-    D=$( echo $DYNO | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g' )
+    D=$( echo "$DYNO" | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g' )
     export DD_HOSTNAME="$HAN.$D"
   else
     # Set the hostname to the dyno host
-    export DD_HOSTNAME=$( echo $DYNOHOST | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
+    DD_HOSTNAME=$( echo "$DYNOHOST" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
+    export DD_HOSTNAME
   fi
 else
   # Generate a warning about DD_HOSTNAME deprecation.
@@ -98,7 +99,7 @@ if [ -n "$DISABLE_DATADOG_AGENT" ]; then
 else
   # Setup Python Path
   DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7"
-  for python_mod_dir in $(ls -d $DD_DIR/embedded/lib/python*/site-packages 2>/dev/null); do
+  for python_mod_dir in $(ls -d "$DD_DIR/embedded/lib/python*/site-packages" 2>/dev/null); do
     DD_PYTHONPATH="${python_mod_dir}:${DD_PYTHONPATH}"
   done
   DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/plat-linux2:$DD_PYTHONPATH"
@@ -109,7 +110,7 @@ else
   # Get the Agent version number
   DD_VERSION=`expr "$($DD_BIN_DIR/agent version)" : 'Agent \([0-9]\+\.[0-9]\+.[0-9]\+\)'`
 
-  # Prior to Agent 2.4.1, the command is "start"
+  # Prior to Agent 6.4.1, the command is "start"
   RUN_VERSION="6.4.1"
   if [ "$DD_VERSION" == "`echo -e "$RUN_VERSION\n$DD_VERSION" | sort -V | head -n1`" ]; then
     RUN_COMMAND="start"
