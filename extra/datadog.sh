58e6da24e4fc1992fc8b5f4b5372cb68fe8428be
Jason Yee @gitbisect <jason.yee@datadoghq.com>
2018-11-08 09:03:00 +0000

    Adding prerun hook, cleaning up bash scripts, better python path handling (#76)
    * making compile and datadog scripts more shellcheck compliant
    
    * Add a prerun hook for users to modify arbitrary env vars and perform actions
    
    * Quotes around variable instances to avoid causing issues
    
    * Expand postgres example
    
    * Add documentation about prerun script
    
    * Provide better python path inclusion

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -20,25 +20,26 @@ export DD_LOG_FILE="$DD_LOG_DIR/datadog.log"
 DD_APM_LOG="$DD_LOG_DIR/datadog-apm.log"
 
 # Move Datadog config files into place
-cp $DATADOG_CONF.example $DATADOG_CONF
+cp "$DATADOG_CONF.example" "$DATADOG_CONF"
 
 # Update the Datadog conf yaml with the correct conf.d and checks.d
-sed -i -e"s|^.*confd_path:.*$|confd_path: $DD_CONF_DIR/conf.d|" $DATADOG_CONF
-sed -i -e"s|^.*additional_checksd:.*$|additional_checksd: $DD_DIR/checks.d|" $DATADOG_CONF
+sed -i -e"s|^.*confd_path:.*$|confd_path: $DD_CONF_DIR/conf.d|" "$DATADOG_CONF"
+sed -i -e"s|^.*additional_checksd:.*$|additional_checksd: $DD_DIR/checks.d|" "$DATADOG_CONF"
 
 # Include application's datadog configs
-APP_DATADOG_CONF_DIR="/app/datadog/conf.d"
+APP_DATADOG="/app/datadog"
+APP_DATADOG_CONF_DIR="$APP_DATADOG/conf.d"
 
 for file in "$APP_DATADOG_CONF_DIR"/*.yaml; do
   test -e "$file" || continue # avoid errors when glob doesn't match anything
-  filename=$(basename -- "$file")
+  filename="$(basename -- "$file")"
   filename="${filename%.*}"
   mkdir -p "$DD_CONF_DIR/conf.d/${filename}.d"
-  cp $file "$DD_CONF_DIR/conf.d/${filename}.d/conf.yaml"
+  cp "$file" "$DD_CONF_DIR/conf.d/${filename}.d/conf.yaml"
 done
 
 # Add tags to the config file
-DYNOHOST="$( hostname )"
+DYNOHOST="$(hostname )"
 DYNOTYPE=${DYNO%%.*}
 TAGS="tags:\n  - dyno:$DYNO\n  - dynotype:$DYNOTYPE"
 
@@ -48,21 +49,21 @@ fi
 
 # Convert comma delimited tags from env vars to yaml
 if [ -n "$DD_TAGS" ]; then
-  DD_TAGS=$(sed "s/,[ ]\?/\\\n  - /g" <<< $DD_TAGS)
+  DD_TAGS="$(sed "s/,[ ]\?/\\\n  - /g" <<< "$DD_TAGS")"
   TAGS="$TAGS\n  - $DD_TAGS"
   # User set tags are now in YAML, clear the env var.
   export DD_TAGS=""
 fi
 
 # Inject tags after example tags.
-sed -i "s/^#   - role:database$/#   - role:database\n$TAGS/" $DATADOG_CONF
+sed -i "s/^#   - role:database$/#   - role:database\n$TAGS/" "$DATADOG_CONF"
 
 # Uncomment APM configs and add the log file location.
-sed -i -e"s|^# apm_config:$|apm_config:\n    log_file: $DD_APM_LOG|" $DATADOG_CONF
+sed -i -e"s|^# apm_config:$|apm_config:\n    log_file: $DD_APM_LOG|" "$DATADOG_CONF"
 
 # Uncomment the Process Agent configs and enable.
 if [ "$DD_PROCESS_AGENT" == "true" ]; then
-  sed -i -e"s|^# process_config:$|process_config:\n    enabled: $DD_PROCESS_AGENT|" $DATADOG_CONF
+  sed -i -e"s|^# process_config:$|process_config:\n    enabled: true|" "$DATADOG_CONF"
 fi
 
 # For a list of env vars to override datadog.yaml, see:
@@ -76,42 +77,52 @@ fi
 if [ -z "$DD_HOSTNAME" ]; then
   if [ "$DD_DYNO_HOST" == "true" ]; then
     # Set the hostname to dyno name and ensure rfc1123 compliance.
-    HAN=$( echo $HEROKU_APP_NAME | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
+    HAN="$(echo "$HEROKU_APP_NAME" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g')"
     if [ "$HAN" != "$HEROKU_APP_NAME" ]; then
       echo "WARNING: The appname \"$HEROKU_APP_NAME\" contains invalid characters. Using \"$HAN\" instead."
     fi
 
-    D=$( echo $DYNO | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g' )
+    D="$(echo "$DYNO" | sed -e 's/[^a-zA-Z0-9.-]/-/g' -e 's/^-//g')"
     export DD_HOSTNAME="$HAN.$D"
   else
     # Set the hostname to the dyno host
-    export DD_HOSTNAME=$( echo $DYNOHOST | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g' )
+    DD_HOSTNAME="$(echo "$DYNOHOST" | sed -e 's/[^a-zA-Z0-9-]/-/g' -e 's/^-//g')"
+    export DD_HOSTNAME
   fi
 else
   # Generate a warning about DD_HOSTNAME deprecation.
   echo "WARNING: DD_HOSTNAME is deprecated. Setting this environment variable may result in metrics errors. To remove it, run: heroku config:unset DD_HOSTNAME"
 fi
 
+# Ensure all check and librariy locations are findable in the Python path.
+DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7"
+# Recursively add packages to python path.
+find "$DD_PYTHONPATH"/embedded/lib/python*/site-packages -type d -exec DD_PYTHONPATH="{}":"$DD_PYTHONPATH" \;
+DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/site-packages:$DD_PYTHONPATH"
+DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/plat-linux2:$DD_PYTHONPATH"
+DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/lib-tk:$DD_PYTHONPATH"
+DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/lib-dynload:$DD_PYTHONPATH"
+DD_PYTHONPATH="$DD_DIR/bin/agent/dist:$DD_PYTHONPATH"
 
+# Give applications a chance to modify env vars prior to running.
+# Note that this can modify existing env vars or perform other actions (e.g. modify the conf file).
+# For more information on variables and other things you may wish to modify, reference this script
+# and the Datadog Agent documentation: https://docs.datadoghq.com/agent
+PRERUN_SCRIPT="$APP_DATADOG/prerun.sh"
+if [ -e "$PRERUN_SCRIPT" ]; then
+  source "$PRERUN_SCRIPT"
+fi
+
+# Execute the final run logic.
 if [ -n "$DISABLE_DATADOG_AGENT" ]; then
   echo "The Datadog Agent has been disabled. Unset the DISABLE_DATADOG_AGENT or set missing environment variables."
 else
-  # Setup Python Path
-  DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7"
-  for python_mod_dir in $(ls -d $DD_DIR/embedded/lib/python*/site-packages 2>/dev/null); do
-    DD_PYTHONPATH="${python_mod_dir}:${DD_PYTHONPATH}"
-  done
-  DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/plat-linux2:$DD_PYTHONPATH"
-  DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/lib-tk:$DD_PYTHONPATH"
-  DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/lib-dynload:$DD_PYTHONPATH"
-  DD_PYTHONPATH="$DD_DIR/bin/agent/dist:$DD_PYTHONPATH"
-
   # Get the Agent version number
-  DD_VERSION=`expr "$($DD_BIN_DIR/agent version)" : 'Agent \([0-9]\+\.[0-9]\+.[0-9]\+\)'`
+  DD_VERSION="$(expr "$($DD_BIN_DIR/agent version)" : 'Agent \([0-9]\+\.[0-9]\+.[0-9]\+\)')"
 
-  # Prior to Agent 2.4.1, the command is "start"
+  # Prior to Agent 6.4.1, the command is "start"
   RUN_VERSION="6.4.1"
-  if [ "$DD_VERSION" == "`echo -e "$RUN_VERSION\n$DD_VERSION" | sort -V | head -n1`" ]; then
+  if [ "$DD_VERSION" == "$(echo -e "$RUN_VERSION\n$DD_VERSION" | sort -V | head -n1)" ]; then
     RUN_COMMAND="start"
   else
     RUN_COMMAND="run"
