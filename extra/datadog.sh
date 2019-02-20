13d9edc463e6f04e1ebb8cb7567b2d0d8d954306
Jason Yee <jason.yee@datadoghq.com>
2019-02-20 15:29:52 -0800

    remove extra colon

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -99,7 +99,7 @@ fi
 # Ensure all check and librariy locations are findable in the Python path.
 DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7"
 # Recursively add packages to python path.
-DD_PYTHONPATH="$DD_PYTHONPATH:$(find "$DD_DIR/embedded/lib/python2.7/site-packages" -maxdepth 1 -type d -printf ":%p")"
+DD_PYTHONPATH="$DD_PYTHONPATH$(find "$DD_DIR/embedded/lib/python2.7/site-packages" -maxdepth 1 -type d -printf ":%p")"
 # Add other packages.
 DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/plat-linux2:$DD_PYTHONPATH"
 DD_PYTHONPATH="$DD_DIR/embedded/lib/python2.7/lib-tk:$DD_PYTHONPATH"
