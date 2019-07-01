5f7ab0ac1a2b8ff14840d2bd0c10cfa960aea63a
Jeremy <jeremy.garcia@datadoghq.com>
2019-07-01 16:37:52 -0400

    fix for #115

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -10,8 +10,8 @@ DATADOG_CONF="$DD_CONF_DIR/datadog.yaml"
 
 # Update Env Vars with new paths for apt packages
 export PATH="$APT_DIR/usr/bin:$DD_BIN_DIR:$PATH"
-export LD_LIBRARY_PATH="$APT_DIR/usr/lib/x86_64-linux-gnu:$APT_DIR/usr/lib:$APT_DIR/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$APT_DIR/usr/lib/x86_64-linux-gnu:$APT_DIR/usr/lib:$LIBRARY_PATH"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$APT_DIR/usr/lib/x86_64-linux-gnu:$APT_DIR/usr/lib:$APT_DIR/opt/datadog-agent/embedded/lib"
+export LIBRARY_PATH="$LIBRARY_PATH:$APT_DIR/usr/lib/x86_64-linux-gnu:$APT_DIR/usr/lib"
 export INCLUDE_PATH="$APT_DIR/usr/include:$APT_DIR/usr/include/x86_64-linux-gnu:$INCLUDE_PATH"
 export PKG_CONFIG_PATH="$APT_DIR/usr/lib/x86_64-linux-gnu/pkgconfig:$APT_DIR/usr/lib/pkgconfig:$PKG_CONFIG_PATH"
 
