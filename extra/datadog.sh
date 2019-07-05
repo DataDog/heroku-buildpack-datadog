33e3bdc010c796a5c896f1418ee1892020c45870
Maciej Mucha <masmoof@gmail.com>
2019-07-05 13:34:07 +0200

    Export DYNOTYPE environment variable

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -40,7 +40,7 @@ done
 
 # Add tags to the config file
 DYNOHOST="$(hostname )"
-DYNOTYPE=${DYNO%%.*}
+export DYNOTYPE=${DYNO%%.*}
 TAGS="tags:\n  - dyno:$DYNO\n  - dynotype:$DYNOTYPE"
 
 if [ -n "$HEROKU_APP_NAME" ]; then
