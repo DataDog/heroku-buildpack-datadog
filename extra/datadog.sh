94f8da0a86c6446855cc97cd697af8f6f0784767
Ara Pulido <ara.pulido@datadoghq.com>
2020-09-09 17:22:11 +0200

    Set buildpack version back to dev

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -45,7 +45,7 @@ done
 # Add tags to the config file
 DYNOHOST="$(hostname )"
 DYNOTYPE=${DYNO%%.*}
-BUILDPACKVERSION="1.20"
+BUILDPACKVERSION="dev"
 DYNO_TAGS="dyno:$DYNO dynotype:$DYNOTYPE buildpackversion:$BUILDPACKVERSION"
 
 if [ -n "$HEROKU_APP_NAME" ]; then
