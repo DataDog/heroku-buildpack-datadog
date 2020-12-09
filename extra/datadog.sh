04ec7bc1be64b268e1976c8d3fd9990dc4c6569b
Ara Pulido <ara.pulido@datadoghq.com>
2020-12-09 13:14:52 +0100

    Prepare 1.22 release

--- extra/datadog.sh
+++ extra/datadog.sh
@@ -49,7 +49,7 @@ done
 # Add tags to the config file
 DYNOHOST="$(hostname )"
 DYNOTYPE=${DYNO%%.*}
-BUILDPACKVERSION="dev"
+BUILDPACKVERSION="1.22"
 DYNO_TAGS="dyno:$DYNO dynotype:$DYNOTYPE buildpackversion:$BUILDPACKVERSION"
 
 # Include install method
