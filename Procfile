c2a87813d09868c8ebc72f76e5d426244d0f02f1
Ara Pulido <ara.pulido@datadoghq.com>
2024-02-22 13:15:17 +0100

    Add different run scripts

---
+++ Procfile
@@ -0,0 +1,4 @@
+tests: .buildpack-testrunner/bin/run .
+tests-with-caching: .buildpack-testrunner/bin/run -c .
+nightly: .buildpack-testrunner/bin/run -s run_nightly_test.sh .
+five: .buildpack-testrunner/bin/run -s run_latest_five_test.sh .
--- test/run_latest_five_test.sh
+++ test/run_latest_five_test.sh
@@ -7,7 +7,7 @@ export DD_LOG_LEVEL="debug"
 testReleased6Versions()
 {
   echo "deb [trusted=yes] https://apt.datadoghq.com/ stable 6" > "${BUILDPACK_HOME}/etc/datadog.list"
-  getAvailableVersions "datadog.list"
+  getAvailableVersions "datadog.list" 5
   echo "deb [signed-by=SIGNED_BY_PLACEHOLDER] https://apt.datadoghq.com/ stable 6" > "${BUILDPACK_HOME}/etc/datadog.list"
 
   for VERSION in ${AGENT_VERSIONS}; do
@@ -23,7 +23,7 @@ testReleased6Versions()
 testReleased7Versions()
 {
   echo "deb [trusted=yes] https://apt.datadoghq.com/ stable 7" > "${BUILDPACK_HOME}/etc/datadog7.list"
-  getAvailableVersions "datadog7.list"
+  getAvailableVersions "datadog7.list" 5
   echo "deb [signed-by=SIGNED_BY_PLACEHOLDER] https://apt.datadoghq.com/ stable 7" > "${BUILDPACK_HOME}/etc/datadog7.list"
 
   for VERSION in ${AGENT_VERSIONS}; do
@@ -36,20 +36,3 @@ testReleased7Versions()
   done
 }
 
-testLatest6NightlyVersion()
-{
-  echo "deb [trusted=yes] http://apt.datad0g.com/ nightly 6" > "${BUILDPACK_HOME}/etc/datadog.list"
-  getAvailableVersions "datadog.list"
-  VERSION="$(echo ${AGENT_VERSIONS} | head -n1 | awk '{print $1;}')"
-
-  compileAndRunVersion $VERSION
-}
-
-testLatest7NightlyVersion()
-{
-  echo "deb [trusted=yes] http://apt.datad0g.com/ nightly 7" > "${BUILDPACK_HOME}/etc/datadog7.list"
-  getAvailableVersions "datadog7.list"
-  VERSION="$(echo ${AGENT_VERSIONS} | head -n1 | awk '{print $1;}')"
-
-  compileAndRunVersion $VERSION
-}
